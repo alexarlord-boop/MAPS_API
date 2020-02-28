f8cc33b342e513690075dff766b3c892d357bb6e
alexarlord-boop <55353828+alexarlord-boop@users.noreply.github.com>
2020-02-28 21:28:34 +0300

    search_update

--- API.py
+++ API.py
@@ -9,17 +9,20 @@ class App(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('UI.ui', self)
-        self.pushButton.clicked.connect(self.update_params)
+        self.pushButton.clicked.connect(self.change_pos_by_lineEdit)
         self.pushButton_2.clicked.connect(self.change_l)
         self.pushButton_3.clicked.connect(self.change_l)
         self.pushButton_4.clicked.connect(self.change_l)
-        self.setWindowTitle('Тест')
+        self.pushButton_5.clicked.connect(self.clear)
+        self.setWindowTitle('MAP')
+
         # self.setGeometry(200, 200, 300, 300)
 
         self.api_key = "40d1649f-0493-4b70-98ba-98533de7710b"
         self.coords = '44.6736646,40.7696272'
         self.zoom = 1
         self.delta = 0
+        self.layer = 'sat'
         self.api_server = f"https://static-maps.yandex.ru/1.x/"
 
         self.params = {
@@ -27,12 +30,11 @@ class App(QMainWindow):
             "z": str(self.zoom),
             "size": '650,450',
             "l": "sat"
-
             # "pt": self.coords + ',pm'
         }
 
     def keyPressEvent(self, event):
-        print('pressed:', event.key())
+        # print('pressed:', event.key())
         if event.key() == 16777238:
             self.change_zoom(1)
         if event.key() == 16777239:
@@ -51,23 +53,45 @@ class App(QMainWindow):
         with open("map.png", 'wb') as f:
             f.write(response.content)
 
-    def change_pos(self, data=None):
-        if data is None:
-            place = ','.join(list(reversed(list(map(str, geocoder.location(self.lineEdit.text()).latlng)))))
-        else:
-            place = data
-        # print(place)
-        self.coords = place
-        self.params["ll"] = self.coords
+    def change_pos_by_lineEdit(self):
+        self.coords = ','.join(list(reversed(list(map(str, geocoder.location(self.lineEdit.text()).latlng)))))
+        print(self.coords)
+        if self.coords == '':
+            self.coords = '90,0'
+        self.params = {
+            "ll": self.coords,
+            "z": str(self.zoom),
+            "size": '650,450',
+            "l": "sat"
+            # "pt": place + ',pm'
+        }
+        self.update_map()
         # self.params["pt"] = self.coords + ',pm2rdm'
+
+    def clear(self):
+        self.lineEdit.setText('')
+        self.change_pos_by_lineEdit()
+        self.lineEdit_2.setText('0')
+        self.change_zoom()
+        self.update_map()
+
+    def change_pos(self, pos):
+        self.coords = pos
+        self.params = {
+            "ll": self.coords,
+            "z": str(self.zoom),
+            "size": '650,450',
+            "l": "sat"
+            # "pt": place + ',pm'
+        }
 
     def change_tile(self, side, k):
         n = 2 ** self.zoom
         tile_size = 180 / n
-        print(n, tile_size)
+        # print(n, tile_size)
         lng, lat = list(map(float, self.coords.split(',')))
         new_coords = self.coords
-        print(lng, lat)
+        # print(lng, lat)
 
         if side == 'h':
             new_lng = lng + k * tile_size
@@ -83,7 +107,7 @@ class App(QMainWindow):
 
         self.change_pos(new_coords)
         self.update_map()
-        print(new_coords)
+        # print(new_coords)
 
     def change_zoom(self, d=0):
         if d == 0:
@@ -92,20 +116,21 @@ class App(QMainWindow):
             self.zoom = self.zoom + d
         if self.zoom not in range(0, 18):
             self.zoom -= d
-            print('back')
-        self.params["z"] = str(self.zoom)
+        self.zoom = self.zoom
         self.lineEdit_2.setText(str(self.zoom))
+        # print(d, self.zoom)
+        self.params["z"] = str(self.zoom)
         self.update_map()
-        print(d, self.zoom)
 
     def change_l(self):
         sender = self.sender()
         if sender.objectName()[-1] == '2':
-            self.params["l"] = 'map'
+            self.layer = 'map'
         if sender.objectName()[-1] == '3':
-            self.params["l"] = 'sat'
+            self.layer = 'sat'
         if sender.objectName()[-1] == '4':
-            self.params["l"] = 'sat,skl'
+            self.layer = 'sat,skl'
+        self.params["l"] = self.layer
         self.update_map()
 
     def load_image(self, file_name):
@@ -115,11 +140,9 @@ class App(QMainWindow):
         self.label.resize(pixmap.width(), pixmap.height())
         # self.resize(pixmap.width(), pixmap.height())
 
-    def update_params(self):
-        self.change_pos()
-        self.change_zoom()
-
     def update_map(self):
+        # self.update_params()
+
         self.get_map()
         self.load_image('map.png')
 
@@ -129,6 +152,6 @@ if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     ex = App()
-    ex.load_image("map.png")
+    ex.update_map()
     ex.show()
     sys.exit(app.exec_())
